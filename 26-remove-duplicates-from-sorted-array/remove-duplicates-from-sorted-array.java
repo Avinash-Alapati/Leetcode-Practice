3ad8d04cef16aab0142c36e29d8a95d4d6c94e96
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2025-11-28 10:07:42 +0530

    Time: 1 ms (63.51%) | Memory: 46.5 MB (31.55%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,14 +1,18 @@
+// BruteForce - TC -> N log N + N , SC -> O(n)
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
+        if (n <= 1){
+            return n;
+        }
 
-        int i = 0;
-        for (int j = 0; j < n; j++){
-            if (nums[j] != nums[i]){
-                nums[i + 1] = nums[j];
-                i++;
+        int idx = 1;
+        for (int i = 1; i < n; i++){
+            if (nums[i] != nums[i - 1]){
+                nums[idx] = nums[i];
+                idx++;
             }
         }
-        return i+1;
+        return idx;
     }
 }
