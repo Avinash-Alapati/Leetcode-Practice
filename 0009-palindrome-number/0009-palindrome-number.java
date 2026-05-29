1804734e35833094ff7c5b051344d433006dfb2c
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2026-05-29 15:49:43 +0530

    Time: 102 ms (6.13%), Space: 46.1 MB (33.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int copy = x;
-        int reversed = 0;
+        int org = x;
+        int temp = org;
+        int rev = 0;
+        while (temp > 0) {
+            int last = temp % 10;
+            rev = rev * 10 + last;
+            temp = temp / 10;
+        } 
 
-        while (x != 0){
-            int lastDigit = x % 10;
-            reversed = reversed * 10 + lastDigit;
-            x = x / 10;
+        if (org < 0) {
+            rev = (-1 * rev);
         }
-        if (copy < 0) {
-           reversed =  (-1 * reversed);
-        }
-        if (copy == reversed) {
-            return true;
-        } else {
-            return false;
-        }
+        return org == rev;
     }
 }
