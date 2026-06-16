98987db46821a1a4ab0b50139c9f75c70755a899
Avinash Alapati <145838055+Avinash-Alapati@users.noreply.github.com>
2026-06-16 11:44:11 +0530

    Time: 11 ms (93.88%), Space: 97.5 MB (12.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       HashMap<Integer,Integer> mp = new HashMap<>();
+    //    return hashMapMethod(nums);
+       return hashSetMethod(nums);
+
+    }
+
+    public boolean hashMapMethod(int[] nums) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++){
             mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
@@ -9,6 +15,17 @@ class Solution {
         for(Map.Entry<Integer,Integer> elem: mp.entrySet()){
             if(elem.getValue() > 1){
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hashSetMethod(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true; 
             }
         }
         return false;
